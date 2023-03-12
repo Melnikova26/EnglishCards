1eee3ff6cae1a26cc1c54fa17387d9be8a79d1bf
Mme1nik <mmelnekowa@gmail.com>
2023-03-12 13:32:38 +0500

    change errors description

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -2,7 +2,7 @@ import img from './error.png';
 const ErrorMessage = () => {
     return (
         <>
-            <h3 style={{ textAlign: 'center', fontSize: '20px'}}>Похоже вышла ошибка. Пожалуйста, перелестните карточку</h3>
+            <h3 style={{ textAlign: 'center', fontSize: '20px'}}>Похоже вышла ошибка. Пожалуйста, повторите попытку позднее!</h3>
             <img style={{ display: 'block', width: "300px", height: "400px", objectFit: 'contain', margin: '0 auto'}} src={img} alt=""Error/>
         </>
 
